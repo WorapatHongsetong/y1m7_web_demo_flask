82c3b94990029725b0f3cf6e60f26a6bd265a862
QuantumTacoNinja <worapat1h@gmail.com>
2025-01-28 10:08:57 +0700

    fix sumsq sum of 2 squares.

---
+++ README.md
@@ -0,0 +1,4 @@
+This is Flask project for Y1M7 HS BKK.
+
+Routes:
+    /
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 @app.route("/sumsq/<value1>/<value2>")
-def test_square(value1, value2):
+def sumsq(value1, value2):
     value1_sq = float(value1) ** 2
     value2_sq = float(value2) ** 2
     sum_of_sq = value1_sq + value2_sq
